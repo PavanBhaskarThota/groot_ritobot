c6f138278318286a17ed33307768129057c751b3
PavanBhaskarThota <pavanbhaskarthota05@gmail.com>
2024-12-07 15:38:15 +0530

    code_submit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Home } from './Pages/Home';
+import { Ricobot } from './Pages/Ricobot';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home/> */}
+      <Ricobot/>
+    </div>
+  );
+}
+
+export default App;
